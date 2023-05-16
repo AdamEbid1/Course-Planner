c7eccd65126e32f6055bb22dfd1e3ec6399aa17d
Adam Ebid <78551691+AdamEbid1@users.noreply.github.com>
2023-05-16 00:47:26 -0400

    Delete dbh.php

--- includes/dbh.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "root";
-$dbName = "courses_db";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-?>
